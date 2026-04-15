e188d2959462bf535cc61dd7d1ab4885a220360a
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-04-15 14:35:37 -0400

    Controller siendo restaurado

--- src/main/java/com/Munoz/Claudio/controller/eventocontroller.java
+++ src/main/java/com/Munoz/Claudio/controller/eventocontroller.java
@@ -1,8 +1,14 @@
 package com.Munoz.Claudio.controller;
-import.com.Munoz.Claudio.service.EventoService;
+importcom.Munoz.Claudio.service.EventoService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Munoz.Claudio.service.eventoService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +20,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("api/v1/eventos")
 
 
-public class Eventocontroller {
+public class eventocontroller {
 @Autowired
-private EventoService eventoService;
+private eventoService eventoService;
 
 @GetMapping("Cargar")
 public String getcargardatos(@RequestParam String param) {
     return new String();
+}
+
+@PostMapping("procesar")
+public ResponseEntity<String> postEventoEntity(@RequestBody String entity) {
+    //TODO: process POST request
+    
+    return ResponseEntity.ok(entity);
+}
+
+@GetMapping
+public List<Evento> getEventos() {
+    return eventoService.readAll();
 }
 
 
